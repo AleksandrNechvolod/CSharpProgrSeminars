aa0ea1c0c5cdb31d1d902a04ab4de862a141dd24
Aleksandr Nechvolod <alexnechvolod@gmail.com>
2023-03-26 13:59:33 +0300

    Added solution into Task08/

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8. Напишите программу, которая
+// на вход принимает число (N), а 
+// на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+ if (count > number)
+{
+  Console.Write($"Это не положительное число, введите положительное!");
+  Console.WriteLine();
+}
+
+while (count <= number)
+{
+  Console.Write($"{count} ");
+  count++;
+}
+
