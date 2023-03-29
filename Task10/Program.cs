8b2e5c51a4255f388704446b3f729907f3e384fa
Aleksandr Nechvolod <alexnechvolod@gmail.com>
2023-03-29 17:21:51 +0300

    Added Task10/ with solution

---
+++ Task10/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 10: Напишите программу, которая
+// принимает на вход трёхзначное число и
+// на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int TernaryCheck(string phrase)
+{
+  Console.Write(phrase);
+  string num = Console.ReadLine();
+  int result = Convert.ToInt32(num);
+  return result;
+}
+
+int ShowSecondDigit(int num)
+{
+  int secondDigit = num / 10 % 10;
+  return secondDigit;
+}
+Console.Clear();
+int number = TernaryCheck("Трехзначное число -> ");
+if (number < 100 || number > 999)
+{
+  Console.Write($"Это не трехзначное число. Введите трехзначное");
+  return;
+}
+int result = ShowSecondDigit(number);
+Console.WriteLine($"Вторая цифра числа ->{result}");
+
+
+
