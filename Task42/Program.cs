1e9fd3187f5b2b3787c6b129d48e05f870a02ed4
Aleksandr Nechvolod <alexnechvolod@gmail.com>
2023-04-12 09:55:24 +0300

    Added Task42/ with solution

---
+++ Task42/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 42:
+//  Напишите программу, которая 
+//  1. будет преобразовывать десятичное число в двоичное.
+
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+string DecToBin(int decimalNumber)
+{
+  string dec = string.Empty;
+  while (decimalNumber > 0)
+  {
+    dec = decimalNumber % 2 + dec;
+    decimalNumber /= 2;
+  }
+  return dec;
+}
+
+Console.Clear();
+int decimalNum = 2;
+string result = DecToBin(decimalNum);
+Console.WriteLine(result);
