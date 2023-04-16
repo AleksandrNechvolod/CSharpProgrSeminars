dba8ef95684e807ab847dd1f77ea122831e4189e
Aleksandr Nechvolod <alexnechvolod@gmail.com>
2023-04-17 00:57:34 +0300

    Added changes into Task48/

--- Task48/Program.cs
+++ Task48/Program.cs
@@ -6,39 +6,6 @@
 // 1 2 3 4
 // 2 3 4 5
 
-// int[,] CreateMatrixSumIndexes(int rows, int columns, )
-// {                          // 0       1
-//     int[,] matrix = new int[rows, columns];
-//     Random rnd = new Random();
-
-//     for (int i = 0; i < matrix.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             matrix[i, j] = i + j;
-//         }
-
-//     }
-//     return matrix;
-// }
-
-// void PrintMatrix(int[,] matrix)
-// {
-//     for (int i = 0; i < matrix.GetLength(0); i++)
-//     {
-//         Console.Write("|");
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             Console.Write($"{matrix[i, j], 4} ");
-//         }
-//         Console.WriteLine("|");
-//     }
-// }
-
-// int[,] array2d = CreateMatrixSumIndexes(3, 4);
-// PrintMatrix(array2d);
-
-
 int[,] CreateMatrixSumIndexes(int rows, int columns)
 {   
     int[,] matrix = new int[rows, columns];
@@ -66,5 +33,6 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
+Console.Clear();
 int[,] array2d = CreateMatrixSumIndexes(3, 4);
 PrintMatrix(array2d);
