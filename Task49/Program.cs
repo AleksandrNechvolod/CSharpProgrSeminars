8d06cf7c67dae6990a4c0b47890f34948521939c
Aleksandr Nechvolod <alexnechvolod@gmail.com>
2023-04-17 01:18:32 +0300

    Added changes into Task49/

--- Task49/Program.cs
+++ Task49/Program.cs
@@ -2,58 +2,6 @@
 //  Найдите элементы, у которых оба индекса чётные, и 
 // замените эти элементы на их на квадраты.
 
-
-
-// int[,] array2d = CreateMatrixRndInt(3, 4, -10, 10);
-// PrintMatrix(array2d);
-// SelectMatrix(array2d);
-// PrintMatrix(array2d);
-// void SelectMatrix(int[,] matrix)
-// {
-//   for (int i = 0; i < matrix.GetLength(0); i += 2)
-//   {
-    
-//     for (int j = 0; j < matrix.GetLength(1); j += 2)
-//      matrix[i,j]
-// }
-// }
-
-
-// int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
-// {                          // 0       1
-//   int[,] matrix = new int[rows, columns];
-//   Random rnd = new Random();
-
-//   for (int i = 0; i < matrix.GetLength(0); i++)
-//   {
-//     for (int j = 0; j < matrix.GetLength(1); j++)
-//     {
-//       matrix[i, j] = rnd.Next(min, max + 1);
-//     }
-
-//   }
-//   return matrix;
-// }
-
-
-int[,] array2d = CreateMatrixRndInt(3, 4, -10, 10);
-PrintMatrix(array2d);
-Console.WriteLine();
-SelectMatrix(array2d);
-PrintMatrix(array2d);
-
-void SelectMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i += 2)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j += 2)
-        {
-            matrix[i, j] *= matrix[i, j];
-        }
-    }
-}
-
-
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {                          // 0       1
     int[,] matrix = new int[rows, columns];
@@ -70,6 +18,17 @@ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
     return matrix;
 }
 
+void ShangeToSquare(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
 void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -82,3 +41,10 @@ void PrintMatrix(int[,] matrix)
         Console.WriteLine("|");
     }
 }
+
+Console.Clear();
+int[,] array2d = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+ShangeToSquare(array2d);
+PrintMatrix(array2d);
