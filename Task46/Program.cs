e161205289e8cc5f3272cfea7ead8e217e8e5d01
Aleksandr Nechvolod <alexnechvolod@gmail.com>
2023-04-17 00:45:23 +0300

    Added changes into Task46/

--- Task46/Program.cs
+++ Task46/Program.cs
@@ -8,32 +8,32 @@
 // 1    5  -2  33  -2
 // 2    77  3  8   1
 
-// int[,] CreateMatrixRndInt(int rows, int columbs, int min, int max)
-// {
-//   int[,] matrix = new int[rows, columbs];
-//   Random rnd = newRandom();
+//  Проход через строки.
+// int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+// {                          // 0       1
+//     int[,] matrix = new int[rows, columns];
+//     Random rnd = new Random();
 
-//   for (int i = 0; i < matrix.GetLength(0); i++)
-//   {
-//     for (int j = 0; j < matrix.GetLength(1);; j++)
+//     for (int i = 0; i < matrix.GetLength(0); i++)
 //     {
-//   matrix[0, 3] = rnd.Next(min, max + 1);
-//      }
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rnd.Next(min, max + 1);
+//         }
+
+//     }
+//     return matrix;
 // }
-// return matrix;
 
-// void PrintMatrix(int[,] matrix)
-
-
-
+// Проход через столбцы.
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {                          // 0       1
     int[,] matrix = new int[rows, columns];
     Random rnd = new Random();
 
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
             matrix[i, j] = rnd.Next(min, max + 1);
         }
