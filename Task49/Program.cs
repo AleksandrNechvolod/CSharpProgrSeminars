0142d96ec4b23c9c909dff00b499fd9e5eaeb651
Aleksandr Nechvolod <alexnechvolod@gmail.com>
2023-04-18 19:34:22 +0300

    Added changes into Task49/ solution

--- Task49/Program.cs
+++ Task49/Program.cs
@@ -4,42 +4,42 @@
 
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {                          // 0       1
-    int[,] matrix = new int[rows, columns];
-    Random rnd = new Random();
+  int[,] matrix = new int[rows, columns];
+  Random rnd = new Random();
 
-    for (int i = 0; i < matrix.GetLength(0); i++)
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = rnd.Next(min, max + 1);
-        }
-
+      matrix[i, j] = rnd.Next(min, max + 1);
     }
-    return matrix;
+
+  }
+  return matrix;
 }
 
 void ShangeToSquare(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i += 2)
+  for (int i = 0; i < matrix.GetLength(0); i += 2)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j += 2)
     {
-        for (int j = 0; j < matrix.GetLength(1); j += 2)
-        {
-            matrix[i, j] *= matrix[i, j];
-        }
+      matrix[i, j] *= matrix[i, j];
     }
+  }
 }
 
 void PrintMatrix(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("|");
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        Console.Write("|");
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write($"{matrix[i, j],4} ");
-        }
-        Console.WriteLine("|");
+      Console.Write($"{matrix[i, j],4} ");
     }
+    Console.WriteLine("|");
+  }
 }
 
 Console.Clear();
