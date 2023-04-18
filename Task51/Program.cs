6226d9ce8baac1531a94b2e025445e59a4680f68
Aleksandr Nechvolod <alexnechvolod@gmail.com>
2023-04-18 20:46:36 +0300

    Added Task51/ with solution

--- Task51/Program.cs
+++ Task51/Program.cs
@@ -7,46 +7,47 @@
 // 8 4 2 4
 // Сумма элементов главной диагонали: 1+9+2 =12
 
-int[,] array2d = CreateMatrixRndInt(4, 5, -10, 10);
-PrintMatrix(array2d);
-Console.WriteLine();
-SelectMatrix(array2d);
-int result = SelectMatrix(array2d);
-Console.Write(result);
-
-int SumDiagonalMatrix(int[,] matrix)
+int SumMainDiagonal(int[,] matrix)
 {
-    int sum = 0;
-    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1) ; i++)
-    {
-        sum = sum + matrix[i, i] ;      
-    }
-   return sum;
+  int sum = 0;
+  int min = matrix.GetLength(0) > matrix.GetLength(1) ? matrix.GetLength(1) : matrix.GetLength(0);
+  for (int i = 0; i < min; i++)
+  {
+    sum += matrix[i, i];
+  }
+  return sum;
 }
 
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {                          // 0       1
-    int[,] matrix = new int[rows, columns];
-    Random rnd = new Random();
-    for (int i = 0; i < matrix.GetLength(0); i++)
+  int[,] matrix = new int[rows, columns];
+  Random rnd = new Random();
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = rnd.Next(min, max + 1);
-        }
+      matrix[i, j] = rnd.Next(min, max + 1);
     }
-    return matrix;
+  }
+  return matrix;
 }
 
 void PrintMatrix(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("|");
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        Console.Write("|");
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write($"{matrix[i, j],4} ");
-        }
-        Console.WriteLine("|");
+      Console.Write($"{matrix[i, j],4} ");
     }
+    Console.WriteLine("|");
+  }
 }
+
+Console.Clear();
+int[,] array2d = CreateMatrixRndInt(3, 5, 0, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+int result = SumMainDiagonal(array2d);
+Console.Write($"Сумма элементов главной диагонали = {result}");
