f009007120c6329cc039512bd4de13bb6c9639f6
Aleksandr Nechvolod <alexnechvolod@gmail.com>
2023-04-20 17:50:18 +0300

    Added Task54/ with solution

---
+++ Task54/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 54.
+// 1. Задайте двумерный массив.
+// 2. Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{                          // 0       1
+  int[,] matrix = new int[rows, columns];
+  Random rnd = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = rnd.Next(min, max + 1);
+    }
+
+  }
+  return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("|");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j],4} ");
+    }
+    Console.WriteLine("|");
+  }
+}
+
+int[,] OrdinalDescendingElementsRows(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+  return array;
+}
+
+Console.Clear();
+int[,] array2d = CreateMatrixRndInt(3, 3, -100, 100);
+PrintMatrix(array2d);
+int[,] newArray2d = OrdinalDescendingElementsRows(array2d);
+Console.WriteLine();
+PrintMatrix(newArray2d);
