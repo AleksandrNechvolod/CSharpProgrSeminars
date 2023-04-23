d2349743b2ce0167a5bdc4edc3fa4a0b349e562a
Aleksandr Nechvolod <alexnechvolod@gmail.com>
2023-04-23 23:14:55 +0300

    Added Task67/ with solution

---
+++ Task67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 67: Напишите программу, которая 
+// будет принимать на вход число и 
+// возвращать сумму его цифр. 
+// 453 -> 12 45 -> 9 
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Сумма цифр данного числа -> {SumDigits(x)}");
+
+int SumDigits(int n)
+{
+  if (n == 0) return 0;
+  else return n % 10 + SumDigits(n / 10);
+}
